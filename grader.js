40dd1861559f445c717a63ab4c22ed7dad2462bc
Ubuntu <ubuntu@ip-172-31-33-72.us-west-2.compute.internal>
2013-07-11 02:10:08 +0000

    added url arguement to grader.js

--- grader.js
+++ grader.js
@@ -3,6 +3,7 @@
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -31,7 +32,25 @@ var checkHtmlFile = function (htmlfile, checksfile) {
 		var present = $(checks[ii]).length > 0;
 		out[checks[ii]] = present;
 	}
-	return out;
+	printJSON(out);
+};
+
+var checkURL = function (url, checksfile) {
+	rest.get(url).on('complete', function (response) {
+		$ = cheerio.load(response);
+		var checks = loadChecks(checksfile).sort();
+		var out = {};
+		for (var ii in checks) {
+			var present = $(checks[ii]).length > 0;
+			out[checks[ii]] = present;
+		}
+		printJSON(out);
+	});
+};
+
+var printJSON = function (checksJson) {
+	var outJson = JSON.stringify(checksJson, null, 4);
+	console.log(outJson);
 };
 
 var clone = function (fn) {
@@ -42,10 +61,16 @@ if (require.main == module) {
 	program
 		.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
 		.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+		.option('-u, --url <url>', 'url to check')
 		.parse(process.argv);
-	var checkJson = checkHtmlFile(program.file, program.checks);
-	var outJson = JSON.stringify(checkJson, null, 4);
-	console.log(outJson);
+
+	// TODO - Error handling / URL assertion
+	if (program.url) {
+		checkURL(program.url, program.checks);
+	}
+	else {
+		checkHtmlFile(program.file, program.checks);
+	}
 }
 else {
 	exports.checkHtmlFile = checkHtmlFile;
